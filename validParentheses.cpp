5194ae45aff1b1db1a8a59d1c7dc285271f65d13
SahilMehar <68806020+SahilMehar@users.noreply.github.com>
2021-10-20 15:18:50 +0530

    Create validParentheses.cpp
    Check whether the string is valid or not

---
+++ validParentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack<char> stk;
+        for(int  i = 0; i < s.size(); i++)
+        {
+            if(s[i] == '{' || s[i] == '[' || s[i] == '(')
+                stk.push(s[i]);
+            else
+            {
+                if(stk.size() == 0)
+                    return 0;
+                if(s[i] == '}' && stk.top() == '{' || s[i] == ']' && stk.top() == '[' || s[i] ==                        ')' && stk.top() == '(')
+                    stk.pop();
+                else
+                    return 0;
+
+            }
+                
+        }
+        if(stk.size() == 0)
+            return 1;
+        else
+            return 0;
+    }
+};
