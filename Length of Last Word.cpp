419d906c41c7ee667884068a569ba6b291466ef3
SahilMehar <68806020+SahilMehar@users.noreply.github.com>
2022-01-30 11:55:40 +0530

    Create Length of Last Word.cpp
    Find the last word of string.

---
+++ Length of Last Word.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) 
+    {
+        int n = s.size()-1, count = 0, ans = 0;
+        int prev = 0;
+        bool flag = false;
+        while(prev <= n)
+        {
+            if(s[n] == ' ' && flag == true)
+                break;
+            if(s[n] != ' ')
+            {
+                count++;
+                flag = true;
+            }
+            n--;
+        }
+           
+        return (count);
+    }
+};
